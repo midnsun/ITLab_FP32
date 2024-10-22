ff1f0db1aaedfe332437f28bd9371954ad870b84
Maksimka <maksimzagradskrv50910@gmail.com>
2024-10-22 22:57:11 +0300

    Changed to FP32
    Multiplication is almost ready

--- main.cpp
+++ main.cpp
@@ -1,67 +1,77 @@
 #include <iostream>
 #include <bitset>
 #include <cmath>
+#include <vector>
+#include <omp.h>
+#include <iomanip>
+#include <random>
+#include <ctime>
+#include <ratio>
+#include <chrono>
 
 using namespace std;
 
-class BF16 {
-	uint16_t getsign() const noexcept {
-		return data >> 15;
+class FP32 {
+	uint32_t getsign() const noexcept {
+		return data >> 31;
 	}
-	int16_t getexp() const noexcept {
-		return (data << 1) >> 8;
+	int32_t getexp() const noexcept {
+		return (data << 1) >> 24;
 	}
-	uint16_t getmantissa() const noexcept {
-		return data & 0x7F;
+	uint64_t getmantissa() const noexcept {
+		return data & 0x007FFFFF;
 	}
-	bool isbfinf() const noexcept {
+	bool isfpinf() const noexcept {
 		return ((getexp() == 0xFF) && getmantissa() == 0);
 	}
-	bool isbfnan() const noexcept {
+	bool isfpnan() const noexcept {
 		return ((getexp() == 0xFF) && getmantissa() != 0);
+	}
+	uint64_t roundDiv(uint64_t a, uint64_t qbits) const noexcept {
+		if (qbits == 0) return a;
+		uint64_t tmp = (a % (uint64_t(1) << qbits)) >= (uint64_t(1) << (qbits - 1));
+		return (a >> qbits) + tmp;
 	}
 public:
 	float example;
-	uint16_t data;
-	BF16() = default;
-	BF16(float f) noexcept {
-		data = reinterpret_cast<uint16_t*>(&f)[1];
+	uint32_t data;
+	FP32() = default;
+	FP32(float f) noexcept {
+		data = reinterpret_cast<uint32_t*>(&f)[0];
 		example = f;
 	}
-	BF16(const BF16& bf) noexcept {
+	FP32(const FP32& bf) noexcept {
 		example = bf.example;
 		data = bf.data;
 	}
-	BF16& operator= (const BF16& bf) noexcept {
+	FP32& operator= (const FP32& bf) noexcept {
 		example = bf.example;
 		data = bf.data;
 		return *this;
 	}
-	BF16& operator= (const uint16_t& bf) noexcept {
+	FP32& operator= (const uint32_t& bf) noexcept {
 		data = bf;
 		example = *this;
 		return *this;
 	}
-	BF16(double d) noexcept {
+	FP32(double d) noexcept {
 		example = static_cast<float>(d);
-		data = reinterpret_cast<uint16_t*>(&example)[1];
+		data = reinterpret_cast<uint32_t*>(&example)[0];
 	}
 	operator float() const noexcept {
-		uint32_t tmp = 0;
-		uint16_t* tmp2 = reinterpret_cast<uint16_t*>(&tmp);
-		tmp2[1] = data;
+		uint32_t tmp = data;
 		return reinterpret_cast<float*>(&tmp)[0];
 	}
-	BF16(uint16_t t) noexcept {
+	FP32(uint32_t t) noexcept {
 		data = t;
 		example = *this;
 	}
 	void print() const noexcept {
-		cout << bitset<16>(data) << endl;
+		cout << bitset<32>(data) << endl;
 	}
 
-	BF16 add(const BF16 l, const BF16 r) /*noexcept*/ {
-		BF16 res;
+	FP32 add(const FP32 l, const FP32 r) /*noexcept*/ {
+		FP32 res;
 		res.example = l.example + r.example;
 		uint16_t sign, normal_unit = 0;
 		//NaN - checking
@@ -106,22 +116,49 @@ public:
 		return res;
 	}
 
-	BF16 mul(const BF16 l, const BF16 r) noexcept {
-		BF16 res;
+	FP32 add2(const FP32 l, const FP32 r) noexcept {
+		FP32 res;
+		res.example = l.example + r.example;
+		uint32_t sign;
+		FP32 fpinf;
+		FP32 fpnan;
+		fpinf.data = 0x7f800000;
+		fpnan.data = 0x7f800001;
+		fpinf.example = INFINITY;
+		fpnan.example = NAN;
+		if (l.isfpnan() || r.isfpnan()) {
+			return fpnan;
+		}
+		if (l.isfpinf() || r.isfpinf()) {
+			return fpnan;
+		}
+		//inf - inf = nan
+
+		int32_t el = l.getexp(), er = r.getexp();
+		uint32_t ml = l.getmantissa(), mr = r.getmantissa();
+
+
+		//передавать в roundDiv число - разность, а потом отбрасывать деление на числа с длиной битов меньше 1?
+
+
+	}
+
+	FP32 mul(const FP32 l, const FP32 r) noexcept {
+		FP32 res;
 		res.example = l.example * r.example;
-		BF16 bfinf;
-		BF16 bfnan;
+		FP32 bfinf;
+		FP32 bfnan;
 		bfinf.data = 0x7f80;
 		bfnan.data = 0x7f80 + 1;
 		bfinf.example = INFINITY;
 		bfnan.example = NAN;
 		bool too_low = false;
-		if (l.isbfinf() || r.isbfinf()) {
-			return bfinf;
-		}
-		if (l.isbfnan() || r.isbfnan()) {
-			return bfnan;
-		}
+		//if (l.isbfinf() || r.isbfinf()) {
+		//	return bfinf;
+		//}
+		//if (l.isbfnan() || r.isbfnan()) {
+		//	return bfnan;
+		//}
 
 		uint16_t sign = l.getsign() ^ r.getsign();
 		uint16_t ml = l.getmantissa(), mr = r.getmantissa();
@@ -168,59 +205,61 @@ public:
 		return res;
 	}
 
-	BF16 mul2(const BF16 l, const BF16 r) const noexcept {
-		BF16 res;
+	FP32 mul2(const FP32 l, const FP32 r) const noexcept { //last bit error in subnormals
+		FP32 res;
+		//res.data = (l.getsign() ^ r.getsign()) << 31;
 		res.data = 0;
 		res.example = l.example * r.example;
-		BF16 bfinf;
-		BF16 bfnan;
-		bfinf.data = 0x7f80 + ((l.getsign() ^ r.getsign()) << 15);
-		bfnan.data = 0x7f81 + ((l.getsign() ^ r.getsign()) << 15);
-		bfinf.example = INFINITY;
-		if (l.getsign() ^ r.getsign()) bfinf.example = -INFINITY;
-		bfnan.example = NAN;
+		FP32 fpinf;
+		FP32 fpnan;
+		fpinf.data = 0x7f800000 + ((l.getsign() ^ r.getsign()) << 31);
+		fpnan.data = 0x7f800001 + ((l.getsign() ^ r.getsign()) << 31);
+		fpinf.example = INFINITY;
+		if (l.getsign() ^ r.getsign()) fpinf.example = -INFINITY;
+		fpnan.example = NAN;
 
-		if (l.isbfinf() || r.isbfinf()) {
-			return bfinf;
+		if (l.isfpinf() || r.isfpinf()) {
+			return fpinf;
 		}
-		if (l.isbfnan() || r.isbfnan()) {
-			return bfnan;
+		if (l.isfpnan() || r.isfpnan()) {
+			return fpnan;
 		}
 
-		uint16_t mres = ((l.getmantissa() + (uint16_t(1) << 7)) * (r.getmantissa() + (uint16_t(1) << 7)) >> 7) - (uint16_t(1) << 7);
-		int16_t eres = l.getexp() + r.getexp() - int16_t(127);
-
-		uint16_t shiftres = mres >> 7;
+		uint64_t mres = (l.getmantissa() + (uint64_t(1) << 23)) * (r.getmantissa() + (uint64_t(1) << 23)); //bad type
+		mres = roundDiv(mres, 23);
+		int32_t eres = l.getexp() + r.getexp() - int32_t(127);
 		
 		if (eres > 0) {
-			res.data += eres << 7;
-			if (shiftres) {
-				res.data += (mres & 0xFF80);
-				res.data += (mres & 0x007F) >> shiftres;
+			while (mres >= (uint64_t(1) << 24)) { //instruction to count 00001***mant zeros can be used
+				++eres;
+				mres >>= 1;
+				//mres = roundDiv(mres, 1); //works correct with simple div
 			}
-			else {
-				res.data += mres;
-			}
-		}
-		else if (eres >= -7) {
+			mres -= uint64_t(1) << 23; //as normals
 			res.data += mres;
+			res.data += eres << 23;
+		}
+		else if (eres >= -23) {
+			mres -= uint64_t(1) << 23; //idk how to reuse normal unit in subnormals
+			res.data += mres/* >> -eres*/;
 			res.data >>= -eres; 
-			res.data += uint16_t(1) << (eres + 7);
+			//mres = roundDiv(mres, -eres); //? correct?
+			res.data += uint32_t(1) << (eres + 23);
 			res.data >>= 1; // +1 as subnormals
+			//mres = roundDiv(mres, 1); //? correct?
 		}
 		else {
-			res.data = 0;
+			res.data += (l.getsign() ^ r.getsign()) << 31; //bad
 			return res;
 		}
-		if (res.data >> 15) {
-			return bfinf;
+		if (res.data >> 31 || res.isfpnan()) {
+			return fpinf;
 		}
-		res.data += (l.getsign() ^ r.getsign()) << 15;
-
+		res.data += (l.getsign() ^ r.getsign()) << 31;
 		return res;
 	}
 
-	BF16& operator+= (const BF16& bf) noexcept {
+	FP32& operator+= (const FP32& bf) noexcept {
 		uint16_t sign;
 		if (getsign() && bf.getsign()) sign = 1;
 		else if (getsign()) {
@@ -234,7 +273,7 @@ public:
 			return *this;
 		}
 	}
-	BF16& operator-= (const BF16& bf) noexcept {
+	FP32& operator-= (const FP32& bf) noexcept {
 		uint16_t sign;
 		if (getsign() && bf.getsign()) sign = 1;
 		else if (getsign()) {
@@ -248,95 +287,194 @@ public:
 			return *this;
 		}
 	}
-	BF16& operator*= (const BF16& bf) noexcept {
+	FP32& operator*= (const FP32& bf) noexcept {
 
 	}
-	BF16& operator/= (const BF16& bf) noexcept {
+	FP32& operator/= (const FP32& bf) noexcept {
 
 	}
-	BF16 operator+ (const BF16& bf) const noexcept {
-		BF16 res = *this;
+	FP32 operator+ (const FP32& bf) const noexcept {
+		FP32 res = *this;
 		res += bf;
 		return res;
 	}
-	BF16 operator- (const BF16& bf) const noexcept {
-		BF16 res = *this;
+	FP32 operator- (const FP32& bf) const noexcept {
+		FP32 res = *this;
 		res -= bf;
 		return res;
 	}
-	BF16 operator* (const BF16& bf) const noexcept {
-		BF16 res = *this;
+	FP32 operator* (const FP32& bf) const noexcept {
+		FP32 res = *this;
 		res *= bf;
 		return res;
 	}
-	BF16 operator/ (const BF16& bf) const noexcept {
-		BF16 res = *this;
+	FP32 operator/ (const FP32& bf) const noexcept {
+		FP32 res = *this;
 		res /= bf;
 		return res;
 	}
 };
 
 class Alltests {
-	BF16 l;
-	BF16 r;
+	FP32 l;
+	FP32 r;
 public:
 	Alltests() = default;
-	bool equal_prec(uint16_t a, uint16_t b, uint16_t prec) {
-		for (uint16_t p = 0; p <= prec; ++p) {
+	bool equal_prec(uint32_t a, uint32_t b, uint32_t prec) {
+		//if (a == 0 && b == 0x80000000 || a == 0x80000000 && b == 0) return true;
+		for (uint32_t p = 0; p <= prec; ++p) {
 			if (a - p == b || a + p == b) return true;
 		}
 		return false;
 	}
-	void run() {
-		uint16_t lc, rc;
-		for (lc = 0x0080; lc < 0x7FFF; lc += 1) {
+	bool run_specific() {
+		vector<uint32_t> vl = { 0x34ebd218, 0x34ebd218 };
+		vector<uint32_t> vr = { 0x001010a0, 0x801010a0 };
+		size_t from = 0;
+		for (size_t i = from; i < vl.size(); ++i) {
+			cout << hex << vl[i] << ", " << vr[i];
+			l = vl[i];
+			r = vr[i];
+			//l = l.add(l, r);
+			l = l.mul2(l, r);
+			//l = l.add2(l, r);
+			//cout << endl << float(BF16(l)) << " " << float(BF16(r)) << endl;
+			//cout << l.example << " " << float(l) << endl;
+			if (/*!isnan(l.example)*/ (l.example == l.example) && !equal_prec(FP32(l.example).data, l.data, 1)) {
+				//cout << " ADD ERROR\n";
+				cout << " MUL ERROR\n";
+				cout << l.example << " expected, " << float(l) << " instead\n";
+				FP32(l.example).print();
+				l.print();
+				return false;
+			}
+			cout << " PASSED\n";
+		}
+		return true;
+	}
+	bool run() {
+		uint64_t lc, rc;
+		for (lc = 0x00800000; lc <= 0xFFFFFFFF; lc += 765432) {
 		//#pragma omp parallel for
-			for (rc = 0x0080; rc < 0x7FFF; rc += 7) {
-				cout << lc << ", " << rc;
-				l = lc;
-				r = rc;
-				//l = l.add(l, r);
+			for (rc = 0x00800000; rc <= 0xFFFFFFFF; rc += 765432) {
+				//cout << hex << lc << ", " << rc;
+				l = uint32_t(lc);
+				r = uint32_t(rc);
+				//l = l.add2(l, r);
 				l = l.mul2(l, r);
 				//cout << endl << float(BF16(l)) << " " << float(BF16(r)) << endl;
 				//cout << l.example << " " << float(l) << endl;
-				if (/*!isnan(l.example)*/ (l.example == l.example) && !equal_prec(BF16(l.example).data, l.data, 1)) {
+				if (/*!isnan(l.example)*/ (l.example == l.example) && !equal_prec(FP32(l.example).data, l.data, 1)) {
 					//cout << " ADD ERROR\n";
+					cout << hex << lc << ", " << rc;
 					cout << " MUL ERROR\n";
 					cout << l.example << " expected, " << float(l) << " instead\n";
-					BF16(l.example).print();
+					FP32(l.example).print();
 					l.print();
-					return;
+					return false;
 				}
-				cout << " PASSED\n";
+				//cout << " PASSED\n";
 			}
 		}
+		return true;
 	}
 };
 
+template <typename T>
+vector <vector <T> > mmul(const vector <vector <T> >& A, const vector <vector <T> >& B) {
+	size_t m = A.size(), n = B.size(), p = B[0].size(); // A: m x n, B: n x p
+	vector <vector <T> > C (m, vector<T>(p)); 
+
+	for (size_t i = 0; i < m; ++i) {
+
+	for (size_t j = 0; j < p; ++j) {
+
+	for (size_t k = 0; k < n; ++k) {
+
+	C[i][j] += A[i][k] * B[k][j];
+
+	}
+	}
+	}
+
+	return C;
+}
+
+vector <vector <double> > mmulv2(const vector <vector <double> >& A, const vector <vector <double> >& B) {
+	size_t m = A.size(), n = B.size(), p = B[0].size(); // A: m x n, B: n x p
+	vector <vector <double> > C(m, vector<double>(p));
+
+	for (size_t i = 0; i < m; ++i) {
+		for (size_t k = 0; k < n; ++k) {
+//#pragma omp parallel for shedul static 6
+			for (size_t j = 0; j < p; ++j) {
+
+
+				C[i][j] += A[i][k] * B[k][j];
+
+			}
+		}
+	}
+
+	return C;
+}
+
 int main() {
-	//uint16_t v;
-	//v = 0x407f;
-	//float a = BF16(v);
-	//BF16(v).print();
-	//v = 0xc001;
-	//float b = BF16(v);
-	//BF16(v).print();
+	//using mat = vector <vector <double> >;
+	//size_t n = 10000;
+	//auto l = [](const mat& m) {
+	//	for (size_t i = 0; i < m.size(); ++i) {
+	//		for (size_t j = 0; j < m[i].size(); ++j) {
+	//			cout << setw(10) << m[i][j];
+	//		}
+	//		cout << endl;
+	//	}
+	//};
+	//auto r = [&n]() {
+	//	mat m = mat(n, vector<double>(n));
+	//	//srand(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()));
+	//	//srand(5);
+	//	//default_random_engine generator;
+	//	random_device rd;
+	//	mt19937 mt(rd());
+	//	uniform_real_distribution<double> dist(-100.0, 100.0);
+	//	for (size_t i = 0; i < n; ++i) {
+	//		for (size_t j = 0; j < n; ++j) {
+	//			m[i][j] = dist(mt);
+	//		}
+	//	}
+	//	return m;
+	//};
 	//
-	//BF16 x = a + b;
-	//x.print();
+	//mat A = {
+	//	{1, 5, 6},
+	//	{6, 3, 8},
+	//	{2, 2, 2},
+	//	{1, 0, -4}
+	//};
+	//mat B = {
+	//	{3, -8, 0, 0},
+	//	{-2, -3, -3, 1},
+	//	{12, 11, 6, -1}
+	//};
 	//
-	//cout << a << " " << b << endl;
-	//cout << a + b << endl;
-	// 
-	//BF16 a = uint16_t(1);
-	//BF16 b = uint16_t(128);
-	//cout << float(a) << endl;
-	//cout << float(b) << endl;
-	//a.add(a,b).print();
-	//cout << a.add(a,b).example << endl;
-
+	//A = r();
+	//B = r();
+	//
+	//cout << "ready\n";
+	//auto start = chrono::high_resolution_clock::now();
+	//
+	//mat C = mmulv2(A, B);
+	//
+	//auto stop = chrono::high_resolution_clock::now();
+	//auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+	//cout << duration.count() << endl;
+	//mat C = mmul(B, A);
+	//l(C);
+	bool flag = true;
 	Alltests tests;
-	tests.run();
-
+	if (flag) flag = tests.run_specific();
+	//if (flag) flag = tests.run();
+	cout << endl << "ENDED" << endl;
 	return 0;
 }
